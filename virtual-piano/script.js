1e49167228050145d1d4958676712fd9b9d86277
Patryk Rogala <rogala.patryk@pm.me>
2022-12-14 13:54:59 +0100

    Stage 5/5: Adding and styling new keys

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,9 +1,14 @@
-const keys = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
+const white_keys = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
+const black_keys = ['w', 'e', 't', 'y', 'u'];
 
 document.addEventListener('keydown', function(event) {
-  if (keys.includes(event.key)) {
+  if (white_keys.includes(event.key)) {
     console.log(`The '${event.key}' key is pressed.`);
     const note = new Audio(`./white_keys/${event.key}.mp3`);
+    note.play();
+  } else if (black_keys.includes(event.key)) {
+    console.log(`The '${event.key}' key is pressed.`);
+    const note = new Audio(`./black_keys/${event.key}.mp3`);
     note.play();
   } else {
     console.log("Please press a valid key");
