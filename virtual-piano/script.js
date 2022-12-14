fc68c26626de58eb2ec62024084b69ad69c32042
Patryk Rogala <rogala.patryk@pm.me>
2022-12-14 13:25:44 +0100

    Stage 4/5: Let there be music

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -3,6 +3,8 @@ const keys = ['a', 's', 'd', 'f', 'g', 'h', 'j'];
 document.addEventListener('keydown', function(event) {
   if (keys.includes(event.key)) {
     console.log(`The '${event.key}' key is pressed.`);
+    const note = new Audio(`./white_keys/${event.key}.mp3`);
+    note.play();
   } else {
     console.log("Please press a valid key");
   }
