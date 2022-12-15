605bdc37784ce06c61f6b775973902b96c069a35
Patryk Rogala <rogala.patryk@pm.me>
2022-12-15 22:40:12 +0100

    Stage 3/5: Guess the age

--- simple-chatty-bot/main.js
+++ simple-chatty-bot/main.js
@@ -4,7 +4,18 @@ console.log("Hello! My name is Aid.");
 console.log("I was created in 2020.");
 console.log("Please, remind me your name.");
 
-// use input() + to get the user's output
-const name = input();
+let name = input();
 
-console.log(`What a great name you have, ${name}!`);
+console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+// reading all remainders
+
+let rem3 = parseInt(input());
+let rem5 = parseInt(input());
+let rem7 = parseInt(input());
+
+let age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+console.log(`Your age is ${age}; that's a good time to start programming!`);
